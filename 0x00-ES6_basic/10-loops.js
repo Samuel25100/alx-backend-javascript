2aa24f6fd7f00729b0f1698d476c36f24f202dc4
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-23 12:00:48 +0300

    add file 10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
+  for (const val of array) {
+    result.push(appendString + val);
+  }
+  return result;
+}
