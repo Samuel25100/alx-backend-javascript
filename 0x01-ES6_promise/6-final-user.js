409a9a21175cfe0078342c89a74c5b3cd118da7e
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-24 15:16:13 +0300

    add file 6-final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,17 +6,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const sign = signUpUser(firstName, lastName);
   const mypro = Promise.allSettled([upload, sign]);
   return mypro.then((result) => {
-    const final = [];
-    for (const val of result) {
-      const obj = {};
-      obj.status = val.status;
-      if (val.status === 'rejected') {
-        obj.value = val.reason;
-      } else {
-        obj.value = val.value;
-      }
-      final.push(obj);
-    }
-    return (final);
+    return (result);
   });
 }
