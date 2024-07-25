e55dd0597f8d6201275403222c6183695c11fa2c
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-25 14:01:10 +0300

    fixing file 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -6,6 +6,22 @@ export default class Pricing {
     this._currency = currency;
   }
 
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
   displayFullPrice() {
     return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
