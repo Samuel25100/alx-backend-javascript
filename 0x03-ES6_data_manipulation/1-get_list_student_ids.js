e7d9358e2fd40b484f58922be0e472c28f277fcf
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-29 10:29:05 +0300

    modify 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,10 +2,6 @@ export default function getListStudentIds(val) {
   if (!(val instanceof Array)) {
     return [];
   }
-  const result = [];
-  val.map((row) => {
-    result.push(row.id);
-    return result;
-  });
+  const result = val.map(row => row.id);
   return result;
 }
