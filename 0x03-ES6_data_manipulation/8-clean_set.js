c9b83502a77664a7e2b8d8ea83ab5012986441ce
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-30 11:40:04 +0300

    fixing file 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,16 @@
 export default function cleanSet(set, start) {
   let result = '';
-  if (!(start.length)) return result;
-  for (const i of set) {
-    if (i.includes(start)) {
-      const re = i.replace(start, '');
-      if (result) result += '-';
-      result += re;
+  try {
+    if (!(start.length)) return result;
+    for (const i of set) {
+      if (i.includes(start)) {
+        const re = i.replace(start, '');
+        if (result) result += '-';
+        result += re;
+      }
     }
+  } catch (err) {
+    return '';
   }
   return result;
 }
