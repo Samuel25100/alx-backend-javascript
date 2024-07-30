b1ca9d46b596af35eb7342bd6f70379eb6619379
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-30 10:16:34 +0300

    fixing file 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8 = new Uint8Array(buffer);
+  const int8 = new Int8Array(buffer);
   try {
     int8[position] = value;
   } catch (err) {
