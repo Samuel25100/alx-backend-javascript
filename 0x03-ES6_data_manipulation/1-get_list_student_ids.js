40e6cb18cedae0baf8f053b6d724d0f916da1def
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-30 10:34:51 +0300

    fixing lint problem for 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,6 @@ export default function getListStudentIds(val) {
   if (!(val instanceof Array)) {
     return [];
   }
-  const result = val.map(row => row.id);
+  const result = val.map((row) => (row.id));
   return result;
 }
