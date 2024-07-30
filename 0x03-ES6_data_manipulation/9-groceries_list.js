f8938f2eb65fa15130ea9b0998e9b6bc16e3e67f
Samuel25100 <samuelgselassie1@gmail.com>
2024-07-30 12:28:25 +0300

    add file  9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const val = new Map();
+  val.set('Apples', 10);
+  val.set('Tomatoes', 10);
+  val.set('Pasta', 1);
+  val.set('Rice', 1);
+  val.set('Banana', 5);
+  return val;
+}
