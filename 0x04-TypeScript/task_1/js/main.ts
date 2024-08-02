639ce781994e61264bef887dd2eee0df12495305
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-02 10:23:13 +0300

    modified main.ts for task-2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,6 +8,10 @@ interface Teacher {
   [key: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -15,3 +19,14 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
+console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
