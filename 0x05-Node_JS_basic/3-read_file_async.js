5a1a366814c3452ebc154cecee9f88fee177f954
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-18 14:51:12 +0300

    add file 3-read_file_async.js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,29 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  let fd;
+  try {
+    fd = await fs.readFile(path, 'utf-8');
+  } catch (err) {
+    throw new Error("Cannot load the database")
+  }
+  const data = fd.split('\n');
+  let parsdata = data.map(line => line.split(','));
+  parsdata.shift()
+  parsdata.pop()
+  let cs = [];
+  let se = [];
+  for (let line of parsdata) {
+    if (line[3] === "CS") {
+      cs.push(line[0]);
+    } else if (line[3] === "SWE") {
+      se.push(line[0]);
+    }
+  }
+
+  console.log(`Number of students: ${parsdata.length}`);
+  console.log(`Number of students in CS: ${cs.length}. List ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${se.length}. List ${se.join(', ')}`);
+}
+
+module.exports = countStudents;
