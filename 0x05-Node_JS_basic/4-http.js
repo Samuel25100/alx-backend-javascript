bc919a0632dffd6d63fa6362daae9c3f66ec25ec
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-18 15:04:52 +0300

    fixing file 4-http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,6 +2,7 @@ const { createServer } = require('http');
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
