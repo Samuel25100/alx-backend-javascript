459a53ece2844a980aab38f10a6bf73913030f6a
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-18 13:10:17 +0300

    add file 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let fd;
+  try {
+    fd = fs.readFileSync(path, 'utf-8');
+  } catch (err) {
+    throw new Error("Cannot load the database");
+  }
+  const data = fd.split('\n');
+  let parsdata = data.map(line => line.split(','));
+  parsdata.shift()
+  parsdata.pop()
+  let cs = [];
+  let se = [];
+  for (let line of parsdata) {
+    if (line[3] === "CS") {
+      cs.push(line[0]);
+    } else if (line[3] === "SWE") {
+      se.push(line[0]);
+    }
+  }
+  console.log(`Number of students: ${data.length - 2}`);
+  console.log(`Number of students in CS: ${cs.length}. List ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${se.length}. List ${se.join(', ')}`);
+}
+
+module.exports = countStudents;
